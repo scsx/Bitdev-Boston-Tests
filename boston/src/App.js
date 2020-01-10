178d2d4d0ec83f50fbe9d4805ad3e0fcfbc06e94
scsx <4182349+scsx@users.noreply.github.com>
2020-01-10 18:31:03 -0500

    Initial commit and Divider cpt

---
+++ boston/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Divider from '@bit/mui-org.material-ui.divider';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Divider variant="middle" />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
