245c9e8dced77873b6fd9202e1a2cce5e918e74e
scsx <4182349+scsx@users.noreply.github.com>
2020-01-17 18:51:51 +0000

    Table added

--- boston/src/App.js
+++ boston/src/App.js
@@ -1,30 +1,145 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+// divider
 import Divider from '@bit/mui-org.material-ui.divider';
 
+// table
+import {withStyles, makeStyles} from '@bit/mui-org.material-ui.styles';
+import Table from '@bit/mui-org.material-ui.table';
+import TableBody from '@bit/mui-org.material-ui.table-body';
+import TableCell from '@bit/mui-org.material-ui.table-cell';
+import TableHead from '@bit/mui-org.material-ui.table-head';
+import TableRow from '@bit/mui-org.material-ui.table-row';
+import Paper from '@bit/mui-org.material-ui.paper';
+
+// Typography
+import Typography from '@bit/mui-org.material-ui.typography';
+
+// table
+const StyledTableCell = withStyles(theme => ({
+    head: {
+        backgroundColor: theme.palette.common.black,
+        color: theme.palette.common.white
+    },
+    body: {
+        fontSize: 14
+    }
+}))(TableCell);
+
+const StyledTableRow = withStyles(theme => ({
+    root: {
+        '&:nth-of-type(odd)': {
+            backgroundColor: theme.palette.background.default
+        }
+    }
+}))(TableRow);
+
+function createData(name, calories, fat, carbs, protein) {
+    return {name, calories, fat, carbs, protein};
+}
+
+const rows = [
+    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+    createData('Eclair', 262, 16.0, 24, 6.0),
+    createData('Cupcake', 305, 3.7, 67, 4.3),
+    createData('Gingerbread', 356, 16.0, 49, 3.9)
+];
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        width: '100%',
+        marginTop: theme.spacing(3),
+        overflowX: 'auto'
+    },
+    table: {
+        minWidth: 700
+    }
+}));
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Divider variant="middle" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const classes = useStyles();
+
+    return (
+        <div className="App">
+            <Typography variant="h1" component="h2" gutterBottom>
+                h1. Heading
+            </Typography>
+            <Typography variant="h2" gutterBottom>
+                h2. Heading
+            </Typography>
+            <Typography variant="h3" gutterBottom>
+                h3. Heading
+            </Typography>
+            <Typography variant="h4" gutterBottom>
+                h4. Heading
+            </Typography>
+            <Typography variant="h5" gutterBottom>
+                h5. Heading
+            </Typography>
+            <Typography variant="h6" gutterBottom>
+                h6. Heading
+            </Typography>
+            <Typography variant="subtitle1" gutterBottom>
+                subtitle1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+                blanditiis tenetur
+            </Typography>
+            <Typography variant="subtitle2" gutterBottom>
+                subtitle2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+                blanditiis tenetur
+            </Typography>
+            <Typography variant="body1" gutterBottom>
+                body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis
+                tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus,
+                cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem
+                quibusdam.
+            </Typography>
+            <Typography variant="body2" gutterBottom>
+                body2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis
+                tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus,
+                cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem
+                quibusdam.
+            </Typography>
+            <Typography variant="button" display="block" gutterBottom>
+                button text
+            </Typography>
+            <Typography variant="caption" display="block" gutterBottom>
+                caption text
+            </Typography>
+            <Typography variant="overline" display="block" gutterBottom>
+                overline text
+            </Typography>
+            <Divider variant="middle"/>
+            <Paper className={classes.root}>
+                <Table className={classes.table} aria-label="customized table">
+                    <TableHead>
+                        <TableRow>
+                            <StyledTableCell>Dessert (100g serving)</StyledTableCell>
+                            <StyledTableCell align="right">Calories</StyledTableCell>
+                            <StyledTableCell align="right">Fat&nbsp;(g)</StyledTableCell>
+                            <StyledTableCell align="right">Carbs&nbsp;(g)</StyledTableCell>
+                            <StyledTableCell align="right">Protein&nbsp;(g)</StyledTableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {rows.map(row => (
+                            <StyledTableRow key={row.name}>
+                                <StyledTableCell component="th" scope="row">
+                                    {row.name}
+                                </StyledTableCell>
+                                <StyledTableCell align="right">{row.calories}</StyledTableCell>
+                                <StyledTableCell align="right">{row.fat}</StyledTableCell>
+                                <StyledTableCell align="right">{row.carbs}</StyledTableCell>
+                                <StyledTableCell align="right">{row.protein}</StyledTableCell>
+                            </StyledTableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </Paper>
+        </div>
+    );
 }
 
 export default App;
